eba5ed6dc4cdeb58eb531cf37c01def802bbf7bf
Ollie Taylor <13766232+OllieJT@users.noreply.github.com>
2021-10-25 17:11:53 +0100

    Fixes npm run dev

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="@sveltejs/kit" />
+
+declare global {
+	namespace NodeJS {
+		interface ProcessEnv {
+			NODE_ENV: "development" | "production";
+			NEXT_PUBLIC_URL: string;
+		}
+	}
+}
