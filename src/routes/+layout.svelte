734f25c2162958d311e24d16d76cdad03a9c869f
Ollie Taylor <13766232+OllieJT@users.noreply.github.com>
2022-09-17 01:22:58 +0100

    adds icon example

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,13 @@
 <script lang="ts">
 	import '@fontsource/inter/400.css';
+	import SvelteIcon from '@inqling/svelte-icons/brand/svelte.svelte';
 	import '../app.css';
 </script>
 
-<slot />
+<header class="p-10">
+	<SvelteIcon class="h-20 w-20 text-orange-500" />
+</header>
+
+<main class="px-10">
+	<slot />
+</main>
