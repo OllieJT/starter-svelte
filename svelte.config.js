cd314f9d6207a65de9284fc65357fb387f6f406f
Ollie Taylor <13766232+OllieJT@users.noreply.github.com>
2023-05-05 14:59:18 +0100

    Update config files

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,24 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: preprocess({ postcss: true }),
+	preprocess: [
+		vitePreprocess(),
+		preprocess({
+			postcss: true,
+		}),
+	],
 
 	kit: {
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
-		alias: {
-			$src: 'src',
-			$lib: 'src/lib',
-		},
+		alias: { $src: 'src' },
 	},
 };
 
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,27 +1,75 @@
+// const colors = require('tailwindcss/colors');
+// const defaultTheme = require('tailwindcss/defaultTheme');
 const plugin = require('tailwindcss/plugin');
-const colors = require('tailwindcss/colors');
-const defaultTheme = require('tailwindcss/defaultTheme');
 
 const config = {
-	content: ['./src/**/*.{html,js,md,mdx,svelte,ts}'],
-	// darkMode: "class",
+	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
 		extend: {
 			fontFamily: {
-				sans: ['Inter var', 'Inter', ...defaultTheme.fontFamily.sans],
+				sans: [
+					'InterVariable',
+					'Inter',
+					// sans stack
+					'ui-sans-serif',
+					'system-ui',
+					'-apple-system',
+					'BlinkMacSystemFont',
+					'"Segoe UI"',
+					'Roboto',
+					'"Helvetica Neue"',
+					'Arial',
+					'"Noto Sans"',
+					'sans-serif',
+					'"Apple Color Emoji"',
+					'"Segoe UI Emoji"',
+					'"Segoe UI Symbol"',
+					'"Noto Color Emoji"',
+				],
 			},
 			colors: {
-				primary: colors.violet,
-				mono: colors.slate,
+				white: '#fff',
+				black: '#000',
+				mono: {
+					50: '#f9fafb',
+					100: '#f3f4f6',
+					200: '#e5e7eb',
+					300: '#d1d5db',
+					400: '#9ca3af',
+					500: '#6b7280',
+					600: '#4b5563',
+					700: '#374151',
+					800: '#1f2937',
+					900: '#111827',
+					950: '#030712',
+				},
+				brand: {
+					50: '#fff7ed',
+					100: '#ffedd5',
+					200: '#fed7aa',
+					300: '#fdba74',
+					400: '#fb923c',
+					500: '#f97316',
+					600: '#ea580c',
+					700: '#c2410c',
+					800: '#9a3412',
+					900: '#7c2d12',
+					950: '#431407',
+				},
 			},
 		},
 	},
 
+	corePlugins: {
+		aspectRatio: false,
+	},
+
 	plugins: [
 		require('@tailwindcss/typography'),
-		require('@tailwindcss/forms'),
 		require('@tailwindcss/aspect-ratio'),
+		require('@tailwindcss/forms'),
+
 		plugin(function ({ addVariant }) {
 			addVariant('hocus', ['&:hover', '&:focus']);
 		}),
