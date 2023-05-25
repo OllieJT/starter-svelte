54faeadbbb1a580c75821bdae0d34bdee2ff51a6
Ollie Taylor <13766232+OllieJT@users.noreply.github.com>
2023-05-25 22:29:23 +0100

    update config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,62 +1,21 @@
-// const colors = require('tailwindcss/colors');
-// const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
+const defaultTheme = require('tailwindcss/defaultTheme');
 const plugin = require('tailwindcss/plugin');
 
+/** @type {import('tailwindcss').Config} */
 const config = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
 		extend: {
 			fontFamily: {
-				sans: [
-					'InterVariable',
-					'Inter',
-					// sans stack
-					'ui-sans-serif',
-					'system-ui',
-					'-apple-system',
-					'BlinkMacSystemFont',
-					'"Segoe UI"',
-					'Roboto',
-					'"Helvetica Neue"',
-					'Arial',
-					'"Noto Sans"',
-					'sans-serif',
-					'"Apple Color Emoji"',
-					'"Segoe UI Emoji"',
-					'"Segoe UI Symbol"',
-					'"Noto Color Emoji"',
-				],
+				sans: ['InterVariable', 'Inter', ...defaultTheme.fontFamily.sans],
 			},
 			colors: {
 				white: '#fff',
 				black: '#000',
-				mono: {
-					50: '#f9fafb',
-					100: '#f3f4f6',
-					200: '#e5e7eb',
-					300: '#d1d5db',
-					400: '#9ca3af',
-					500: '#6b7280',
-					600: '#4b5563',
-					700: '#374151',
-					800: '#1f2937',
-					900: '#111827',
-					950: '#030712',
-				},
-				brand: {
-					50: '#fff7ed',
-					100: '#ffedd5',
-					200: '#fed7aa',
-					300: '#fdba74',
-					400: '#fb923c',
-					500: '#f97316',
-					600: '#ea580c',
-					700: '#c2410c',
-					800: '#9a3412',
-					900: '#7c2d12',
-					950: '#431407',
-				},
+				mono: colors.slate,
+				brand: colors.orange,
 			},
 		},
 	},
