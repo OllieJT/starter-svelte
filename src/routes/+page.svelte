f1374a7f5ff5dd2cbfa012ac363aa18f2ff3bf16
Ollie Taylor <13766232+OllieJT@users.noreply.github.com>
2023-08-26 14:03:16 +0100

    Remove markdown

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,14 +11,4 @@
 <a class="text-primary-500" href="/nested-page">Go to nested page -></a>
 <a class="text-primary-500" href="/form">Go to form demo -></a>
 
-<ul>
-	{#each data.articles as article}
-		<li>
-			<a href="/{article.slug}" class="font-medium text-purple-700 hover:underline">
-				{article.metadata.title}
-			</a>
-		</li>
-	{/each}
-</ul>
-
 <pre class="my-10 border-t border-mono-500/50 py-10">{JSON.stringify(data, null, 4)}</pre>
